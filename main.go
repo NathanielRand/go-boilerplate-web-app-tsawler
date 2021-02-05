e4b226f79e3ac32f287ab0414b738d3da1e36d15
NathanielRand <nathanieljrand@gmail.com>
2021-02-05 05:24:07 +0000

    Added about route

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About")
+}
+
 func main() {
 	// Routes
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
 
 	// 	Server
 	http.ListenAndServe(":80", nil)
