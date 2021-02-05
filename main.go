e1579e8bd435951dd536d1c3c055022b4b3e4bc6
NathanielRand <nathanieljrand@gmail.com>
2021-02-05 05:27:07 +0000

    Add contact route

--- main.go
+++ main.go
@@ -13,10 +13,15 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About")
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contact")
+}
+
 func main() {
 	// Routes
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", about)
 
 	// 	Server
 	http.ListenAndServe(":80", nil)
